5805b309481f04ef56de009b165f6402c7efc33b
rembe92 <40696432+rembe92@users.noreply.github.com>
2019-06-19 17:43:13 +0200

    SetDeleteCube => setDeleteCube

--- Assets/Scripts/setDeleteCube.cs
+++ Assets/Scripts/setDeleteCube.cs
@@ -2,7 +2,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SetDeleteCube : MonoBehaviour
+public class setDeleteCube : MonoBehaviour
 {
 
     private List<GameObject> cubes = new List<GameObject>();
